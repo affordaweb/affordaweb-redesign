0ca0c9d7378ed8eaf3d301c799aaf4970bfa9202
affordaweb <affordawebsolutions@gmail.com>
2026-04-08 05:32:04 -0700

    chore: update IndexNow script with www host and all new URLs

--- indexnow-submit.js
+++ indexnow-submit.js
@@ -3,23 +3,42 @@
 // Run after deploy: node indexnow-submit.js
 
 const KEY = 'fad4f379663a2065610d5e3a07cec7bf'
-const HOST = 'affordawebsolutions.com'
+const HOST = 'www.affordawebsolutions.com'
 const BASE = `https://${HOST}`
 
 const urls = [
   `${BASE}/`,
   `${BASE}/about`,
   `${BASE}/services`,
+  `${BASE}/services/design`,
+  `${BASE}/services/redesign`,
+  `${BASE}/services/seo`,
+  `${BASE}/services/maintenance`,
+  `${BASE}/services/hosting`,
   `${BASE}/pricing`,
   `${BASE}/portfolio`,
   `${BASE}/blog`,
   `${BASE}/contact`,
+  `${BASE}/philadelphia`,
+  `${BASE}/houston`,
+  `${BASE}/los-angeles`,
+  `${BASE}/fresno`,
+  `${BASE}/new-jersey`,
+  `${BASE}/affordable-web-design-company`,
+  `${BASE}/affordable-web-design-for-small-businesses`,
   `${BASE}/blog/why-small-businesses-need-professional-website`,
   `${BASE}/blog/affordable-website-design-vs-diy-website-builder`,
   `${BASE}/blog/seo-tips-small-business-website`,
   `${BASE}/blog/website-design-subscription-model-explained`,
   `${BASE}/blog/website-speed-matters-small-business`,
-  `${BASE}/blog/local-seo-guide-small-businesses`,
+  `${BASE}/blog/how-to-choose-web-design-company-small-business`,
+  `${BASE}/blog/website-maintenance-why-it-matters`,
+  `${BASE}/blog/mobile-website-design-guide`,
+  `${BASE}/blog/how-much-does-a-website-cost`,
+  `${BASE}/blog/who-updates-a-website-after-the-web-designer-finishes`,
+  `${BASE}/blog/website-maintenance-requirements`,
+  `${BASE}/blog/web-design-vs-web-hosting`,
+  `${BASE}/blog/web-design-competitor-comparison`,
 ]
 
 async function submit() {
